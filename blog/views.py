eacc163516b24e7837109491b5c99569f07b9062
Prashant Singh <prashantsingh5aug@gmail.com>
2023-01-26 23:16:24 +0530

    User Interface is created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from blog.models import Blog
+from django.contrib import messages
 
 # Create your views here.
+def home(req):
+    if req.method=="POST":
+        title = req.POST['title']
+        content = req.POST['content']
+
+        if(len(title)<2 or len(content)<11):
+            messages.error(req, 'Please fill the form correctly (Minimum length of title and content should be at least 2 and 11 respectively)!')
+        else:
+            blog = Blog(title=title, content=content)
+            blog.save()
+            messages.success(req, 'Your blog has been posted suffessfully!')
+    return render(req, 'home.html')
--- multilangBlog/settings.py
+++ multilangBlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'multilangBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
