9308059eddc1bd72979370f2eaee5da38de95857
Prashant Singh <prashantsingh5aug@gmail.com>
2023-01-28 14:11:03 +0530

    Multiple language support is added

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from blog.models import Blog
+from blog.models import Blog, Language
 
 # Register your models here.
 admin.site.register(Blog)
+admin.site.register(Language)
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 # Create your models here.
+LANGUAGES = {"en":"English","fr":"French"}
+
+class Language(models.Model):
+   code = models.CharField(max_length=7, primary_key=True)
+   language = models.CharField(max_length=100)
+ 
+   def __str__(self):
+       return f'{self.code}'
+
 class Blog(models.Model):
     sno = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+    slug = models.CharField(max_length=255)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, default=False, null=True)
+    title = models.TextField()
 
     def __str__(self):
         return self.title
